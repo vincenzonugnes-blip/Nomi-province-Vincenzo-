34e17947929640d06ceb18e3b3754d04f8bd5ead
vincenzo nugnes <vincenzonugnes@gmail.com>
2026-04-20 09:36:37 +0200

    Upgrade service worker cache version and logic
    Updated service worker to use cache version v3 and modified fetch handling.

--- sw.js
+++ sw.js
@@ -1,12 +1,18 @@
-const CACHE = 'renovit-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'renovit-v3';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
